023606e3046ac8a4060a48627c947cff0573065a
nikhil0307 <vishalnikhil0307@gmail.com>
2025-05-28 23:49:17 +0530

    disabled eslint

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // Your other Next.js configurations can go here
-  // For example: reactStrictMode: true,
-
-  // Add allowedDevOrigins here, at the top level
-  allowedDevOrigins: ["http://localhost:3000", "http://192.168.29.241:3000"], // Add your dev machine's IP if accessing from another device
+  // ... your other configurations
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
